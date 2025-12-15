45d3d8931b44df973edfda953fdef46744b3c078
Zamir Hashorva <zamir.hashorva@gmail.com>
2025-12-15 19:05:27 +0100

    feat(soup): build the movies.txt with a simple script that is memory efficient

---
+++ day_45/Movie_Challenge/main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+EMPIRE_URL = 'https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/'
+
+def main():
+    response = requests.get(EMPIRE_URL)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    rows = soup.find_all(name="h3", class_="title")
+
+    # To reverse the movies use the slice [::-1] or the reverse() object as list(reversed(movies))
+    movies = [ row.getText() for row in rows ]
+    movies = movies[::-1] # Reverse with slicing
+
+    # Take each movie and write it to the file
+    with open("./movies.txt", "w", encoding="utf-8") as file:
+        for movie in movies:
+            file.write(movie + "\n")
+
+    print(len(movies))
+
+if __name__ == "__main__":
+    main()
