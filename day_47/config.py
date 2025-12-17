c96b901b955be07e566674fa6428efc15b21bc1a
Zamir Hashorva <zamir.hashorva@gmail.com>
2025-12-17 20:20:55 +0100

    (day_47): Built get_price() that returns currency and price

--- day_47/config.py
+++ day_47/config.py
@@ -2,3 +2,8 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+# --- Testing Page ---
+BREWERY_URL = "https://appbrewery.github.io/instant_pot/"
+AMAZON_URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+USER_AGENT = os.getenv("USER_AGENT")
--- day_47/main.py
+++ day_47/main.py
@@ -1,7 +1,24 @@
-# Day 47 - Created automatically
+from bs4 import BeautifulSoup
+from config import (
+    BREWERY_URL,
+)
+import requests
+
+def get_price():
+    url = BREWERY_URL
+    response = requests.get(url=url)
+    print(response.status_code)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    full_price = soup.find(class_="aok-offscreen").get_text().split()[0]
+    currency = full_price[0]
+    price = float(full_price[1:])
+
+    return currency, price
 
 def main():
-    print('Hello Day 47')
+    print(get_price())
 
 if __name__ == '__main__':
     main()
