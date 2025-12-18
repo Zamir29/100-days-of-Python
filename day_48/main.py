c036bc0e89fc459e95a8ff527253bbe2da0d1542
Zamir Hashorva <zamir.hashorva@gmail.com>
2025-12-18 01:40:43 +0100

    (day_48): tested webdirever commands

--- day_48/main.py
+++ day_48/main.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
 
 def main():
-    driver = webdriver.Chrome()
+    # Keep Chrome browser open after program finishes
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.amazon.com")
+
+    # driver.close() # Close a tab
+    # driver.quit() # Quit the entire program
 
 if __name__ == '__main__':
     main()
