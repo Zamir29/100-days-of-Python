3a6b6911963b7ed114d150a5e2ea6f65b0361576
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-05 14:07:02 +0100

    (day_50): Added helpers and the TimeoutException to handle wait for like button

--- day_50/main.py
+++ day_50/main.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from selenium.common.exceptions import (
+    ElementClickInterceptedException,
+    TimeoutException,
+)
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from time import sleep
@@ -14,7 +17,7 @@ def wait_click(wait, by, selector):
     return wait.until(ec.element_to_be_clickable((by, selector))).click()
 
 def wait_find(wait, by, selector):
-    return wait.until(ec.presence_of_element_located((by, selector)))
+    return wait.until(ec.visibility_of_element_located((by, selector)))
 
 def main():
     driver = webdriver.Chrome()
@@ -22,15 +25,21 @@ def main():
 
     driver.get("https://www.tinder.com")
 
-    login_button = wait.until(
-        ec.element_to_be_clickable((By.XPATH, '//*[text()="Log In"]'))
-    )
-    login_button.click()
+    # login_button = wait.until(
+    #     ec.element_to_be_clickable((By.XPATH, '//*[text()="Log In"]'))
+    # )
+    # login_button.click()
 
-    fb_login = wait.until(
-        ec.element_to_be_clickable((By.XPATH, '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button'))
-    )
-    fb_login.click()
+    # Wait and click Log-in Button
+    wait_click(wait, By.XPATH, '//*[text()="Log In"]')
+
+    # fb_login = wait.until(
+    #     ec.element_to_be_clickable((By.XPATH, '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button'))
+    # )
+    # fb_login.click()
+
+    # Wait and click Facebook Log-in button
+    wait_click(wait, By.XPATH, '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
 
     # Select Facebook login window pop up
     wait.until(ec.number_of_windows_to_be(2))
@@ -42,30 +51,45 @@ def main():
     print(driver.title)
 
     # Login and click enter
-    email = wait.until(
-        ec.visibility_of_element_located((By.XPATH, '//*[@id="email"]'))
-    )
-    password = wait.until(
-        ec.visibility_of_element_located((By.XPATH, '//*[@id="pass"]'))
-    )
+    # email = wait.until(
+    #     ec.visibility_of_element_located((By.XPATH, '//*[@id="email"]'))
+    # )
+    email = wait_find(wait, By.XPATH, '//*[@id="email"]')
+
+    # password = wait.until(
+    #     ec.visibility_of_element_located((By.XPATH, '//*[@id="pass"]'))
+    # )
+    password = wait_find(wait, By.XPATH, '//*[@id="pass"]')
+
     email.send_keys(FACEBOOK_EMAIL)
     password.send_keys(FACEBOOK_PASSWORD)
+
     password.send_keys(Keys.ENTER)
 
     # Switch back to Tinder window
     driver.switch_to.window(base_window)
     print(driver.title)
 
-    sleep(5)
+    try:
+        wait_click(wait, By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+    except TimeoutException:
+        pass # it might not show
+    # allow_location_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+    # allow_location_button.click()
 
-    allow_location_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]]')
-    allow_location_button.click()
+    try:
+        wait_click(wait, By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+    except TimeoutException:
+        pass # it might not show
+    # notifications_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+    # notifications_button.click()
 
-    notifications_button = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
-    notifications_button.click()
-
-    cookies_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-    cookies_button.click()
+    try:
+        wait_click(wait, By.XPATH, '//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+    except TimeoutException:
+        pass # it might not show
+    # cookies_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+    # cookies_button.click()
 
     # Tinder limit 100:
     for n in range(100):
@@ -73,20 +97,28 @@ def main():
         sleep(1)
 
         try:
-            print("called")
-            like_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
-            like_button.click()
+            print(f"Swipe {n+1}/100")
+            # like_button = driver.find_element(By.XPATH, value='//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+            # like_button.click()
+
+            # Click like button with wait
+            wait_click(wait, By.XPATH, '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
 
         # If It's a Match window pops up
         except ElementClickInterceptedException:
             try:
-                match_popup = driver.find_element(By.CSS_SELECTOR, value='.itsAMatch a')
-                match_popup.click()
+                # match_popup = driver.find_element(By.CSS_SELECTOR, value='.itsAMatch a')
+                # match_popup.click()
+
+                wait_click(wait, By.CSS_SELECTOR, '.itsAMatch a')
 
             # Catches the cases where the "Like" button has not yet loaded
-            except NoSuchElementException:
-                sleep(2)
+            except TimeoutException:
+                pass
 
+        except TimeoutException:
+            # Like button didn't become clickable in time
+            pass
     driver.quit()
 
 if __name__ == '__main__':
