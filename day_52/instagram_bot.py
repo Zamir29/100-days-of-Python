48b2fb4d2ddd0bf9c6427889b449a14184970af6
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-08 17:14:02 +0100

    (day_52): build find_followers method but not tested, out of scope

--- day_52/instagram_bot.py
+++ day_52/instagram_bot.py
@@ -3,13 +3,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from config import (
     INSTA_USERNAME,
     INSTA_PASSWORD,
-    # SIMILAR_ACCOUNT,
+    SIMILAR_ACCOUNT,
 )
 class InstaFollower:
     def __init__(self):
@@ -22,10 +22,11 @@ class InstaFollower:
         self.driver = webdriver.Chrome(options=chrome_options)
         self.timeout_default = 10
         self.wait = WebDriverWait(self.driver, self.timeout_default)
-        self.url = "https://www.instagram.com/accounts/login/"
+        self.insta_url = "https://www.instagram.com/"
+        self.insta_login = f"{self.insta_url}accounts/login/"
 
     def login(self):
-        self.driver.get(self.url)
+        self.driver.get(self.insta_login)
         try:
             decline_cookies = self.wait.until(
                 ec.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Decline')]"))
@@ -40,12 +41,14 @@ class InstaFollower:
             ec.element_to_be_clickable((By.NAME, 'username'))
         )
         username.click()
+        username.clear()
         username.send_keys(INSTA_USERNAME)
 
         password = self.wait.until(
             ec.element_to_be_clickable((By.NAME, 'password'))
         )
         password.click()
+        password.clear()
         password.send_keys(INSTA_PASSWORD)
         password.send_keys(Keys.RETURN)
         time.sleep(5)
@@ -60,9 +63,41 @@ class InstaFollower:
             except TimeoutException:
                 break
 
-
     def find_followers(self):
-        pass
+        # 1) Go to the target profile
+        self.driver.get(f"{self.insta_url}{SIMILAR_ACCOUNT}/")
+
+        # 2) Click followers link
+        followers_button = self.wait.until(
+            ec.element_to_be_clickable((By.XPATH, "//a[contains(@href,'/followers/')]"))
+        )
+        followers_button.click()
+
+        # 3) Wait for the followers modal (dialog) to appear
+        dialog = self.wait.until(
+            ec.presence_of_element_located((By.XPATH, "//div[@role='dialog']"))
+        )
+
+        # 4) Find the scrollable container *inside* the dialog
+        # This targets the div that actually scrolls (overflow-y: auto / scroll).
+        try:
+            scroll_box = dialog.find_element(
+                By.XPATH,
+                ".//div[contains(@style,'overflow') and contains(@style,'auto')]"
+            )
+        except NoSuchElementException:
+            scroll_box = dialog.find_element(By.XPATH, ".//div[contains(@style,'overflow')]")
+
+        # 5) Scroll the modal several times to load followers
+        for _ in range(10):
+            self.driver.execute_script(
+                "arguments[0].scrollTop = arguments[0].scrollHeight",
+                scroll_box
+            )
+            time.sleep(1.2)
+
+        self.dialog = dialog
+        self.scroll_box = scroll_box
 
     def follow(self):
         pass
