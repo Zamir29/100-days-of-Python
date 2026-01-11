4df921c6f7ac8b3d53ef4e100ff43ae3183fe4aa
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-11 14:42:29 +0100

    (day_53): added the form filler and tested succesfully

--- day_53/scraper_bot.py
+++ day_53/scraper_bot.py
@@ -1,12 +1,13 @@
 import requests
 import re
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import TimeoutException
+# from selenium.common.exceptions import TimeoutException
 
 class Scraper:
     def __init__(self, url: str, headers: dict, timeout: int = 15):
@@ -18,15 +19,14 @@ class Scraper:
     def fetch(self) -> str:
         response = requests.get(url=self.url, headers=self.headers, timeout=self.timeout)
         response.raise_for_status()
-        site_html = response.text
 
-        return site_html
+        return response.text
 
     @staticmethod
-    def parse(html: str):
+    def parse(html: str) -> BeautifulSoup:
         return BeautifulSoup(html, "html.parser")
 
-    def get_list(self, soup):
+    def get_list(self, soup: BeautifulSoup) -> list[dict[str, str]]:
         result_container = soup.select_one(selector="div.result-list-container ul")
 
         if not result_container:
@@ -36,7 +36,7 @@ class Scraper:
         # Get only the direct child of <ul> https://beautiful-soup-4.readthedocs.io/en/latest/index.html?highlight=find_all#the-recursive-argument
         item_list = result_container.find_all("li", recursive=False)
 
-        all_items = []
+        all_items: list[dict[str, str]] = []
         for item in item_list:
             data = self.get_item_data(item)
             if data is not None:
@@ -45,7 +45,8 @@ class Scraper:
         return all_items
 
 
-    def get_item_data(self, one_item):
+    def get_item_data(self, one_item) -> dict[str, str] | None:
+
         # Focus on the <a> tag that contains link and address
         link_tag = one_item.select_one("a[data-test='property-card-link']")
 
@@ -79,31 +80,83 @@ class Scraper:
         number_str = match.group(1)
         price_text = f"{self.currency}{number_str}"
 
-        data_dict = {
+        return {
             "address": address_text,
             "price": price_text,
             "link": listing_url,
         }
 
-        return data_dict
 
     def run(self) -> list[dict[str, str]]:
         html = self.fetch()
-        soup = self.parse(html=html)
-        return self.get_list(soup=soup)
+        soup = self.parse(html)
+        return self.get_list(soup)
 
 class FormFiller:
-    def __init__(self, url: str, headers: dict, timeout: int = 15):
+    def __init__(self, url: str, timeout: int = 15):
         self.url = url
-        self.headers = headers
         self.timeout = timeout
 
     def create_webdriver(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option(name="detach", value=True)
+        chrome_options.add_experimental_option("detach", True)
 
         driver = webdriver.Chrome(options=chrome_options)
         wait = WebDriverWait(driver, self.timeout)
 
         return driver, wait
 
+    def fill_form(self, data_dict: dict[str, str]):
+
+        # Map Google Form field labels -> your dict keys
+        align_params = {
+            "address": "address",
+            "price/month": "price",
+            "link": "link",
+        }
+
+        # Create driver and get Google Form url
+        driver, wait = self.create_webdriver()
+        driver.get(self.url)
+
+        # Wait for the list of questions to be visible
+        form_list = wait.until(
+            ec.visibility_of_element_located(
+                (By.CSS_SELECTOR, "form[method='post'] div[role='list']")
+            )
+        )
+
+        # Create a list of the items to be filled
+        form_items = form_list.find_elements(
+            By.CSS_SELECTOR, "div[role='listitem']"
+        )
+
+        # Loops through items
+        for item in form_items:
+
+            #Find the label and compare to the Map
+            label = item.find_element(By.CSS_SELECTOR, "div[role='heading'] span").text.strip().lower()
+            print("Field label:", label)
+
+            if label not in align_params:
+                continue
+
+            key = align_params[label]
+            value = data_dict.get(key, "")
+
+            input_item = item.find_element(By.CSS_SELECTOR, "input")
+            input_item.click()
+            input_item.send_keys(value)
+
+        buttons = wait.until(
+            ec.element_to_be_clickable(
+                (By.CSS_SELECTOR, "div[aria-label='Submit']")
+            )
+        )
+
+        buttons.click()
+        time.sleep(0.2)
+
+        driver.quit()
+
+        return
