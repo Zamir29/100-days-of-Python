7b7256ecbabc4c3b652cec95a2413c0815afaabe
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-12 19:01:26 +0100

    (day_54): imported flask and run the first server

--- day_54/main.py
+++ day_54/main.py
@@ -1,7 +1,5 @@
-# Day 54 - Created automatically
-
-def main():
-    print('Hello Day 54')
-
-if __name__ == '__main__':
-    main()
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
