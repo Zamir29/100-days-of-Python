8f877973a23a2e66412bc245efc0ee9e61e7b00d
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-13 12:01:02 +0100

    (day_54): learned about decorators and flask tools

--- day_54/main.py
+++ day_54/main.py
@@ -3,3 +3,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+if __name__ == "__main__":
+    app.run(debug=True)
