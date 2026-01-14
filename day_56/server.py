cbbab7e422e943ada26d5f4bedaccd25aa469ed0
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-14 11:07:38 +0100

    (day_56): created the cv html to test the routs of Flask

--- day_56/server.py
+++ day_56/server.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/cv/zamir')
+def cv():
+    return render_template('cv/zamir.html')
+
 def main():
     app.run(debug=True) #, host='0.0.0.0', port=5000)
 
