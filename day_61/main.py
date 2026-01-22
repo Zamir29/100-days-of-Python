c72c39f11d505d405fb4a4f7c44cfc68202ffc44
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-22 10:14:13 +0100

    (day_61): added route login to main.py

--- day_61/main.py
+++ day_61/main.py
@@ -20,10 +20,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """
-    This is home 
+    This is home
     """
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    """
+    Login page
+    """
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
