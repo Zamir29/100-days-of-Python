1b8b403a46f35ad7e2edb9a4026adaf4b91fd305
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-22 11:46:36 +0100

    (day_61): added and tested StringField and PasswordField with error return

--- day_61/main.py
+++ day_61/main.py
@@ -10,11 +10,22 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
-from flask import Flask, render_template
-
-
+from h11 import Data
+from config import (
+    WTF_CSRF_SECRET_KEY,
+)
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = WTF_CSRF_SECRET_KEY
+class LoginForm(FlaskForm):
+    ''' Login form '''
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()] )
+
 
 
 @app.route("/")
@@ -24,12 +35,15 @@ def home():
     """
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     """
     Login page
     """
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        return "Form is valid"
+    return render_template("login.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
