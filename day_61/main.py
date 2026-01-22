64c3402557559b9fb4f6946a7ce7df1e0ae30ad7
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-22 16:26:11 +0100

    (day_61): rebuild the website with template inheritance

--- day_61/main.py
+++ day_61/main.py
@@ -16,15 +16,19 @@ from config import (
 )
 from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.secret_key = WTF_CSRF_SECRET_KEY
+
+EMAIL = "admin@testing.test"
+PASSWORD = "1234test"
 class LoginForm(FlaskForm):
     ''' Login form '''
-    email = StringField('email', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()] )
+    submit = SubmitField(label='Log in')
 
 
 
@@ -42,8 +46,23 @@ def login():
     """
     form = LoginForm()
     if form.validate_on_submit():
-        return "Form is valid"
+        if form.email.data == EMAIL and form.password.data == PASSWORD:
+            return redirect(url_for("success"))
+        else:
+            return redirect(url_for("denied"))
+        # print(form.email.data)
+        # return "Form is valid"
     return render_template("login.html", form=form)
+
+@app.route("/login/success")
+def success():
+    '''Go to success page in case you made it'''
+    return render_template("success.html")
+
+@app.route("/login/denied")
+def denied():
+    '''Go to denied page in case you failed it'''
+    return render_template("denied.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
