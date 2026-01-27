53b4038e6ff2b841c9ca676040a9fc6bd916ea87
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-27 16:37:03 +0100

    (day_63): add SQLAlchemy modl and /add route to the day_63 project

--- day_63/main.py
+++ day_63/main.py
@@ -12,16 +12,37 @@ This will install the packages from requirements.txt for this project.
 '''
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 
+# SQLAlchemy setup
+class Base(DeclarativeBase):
+    """ Use the class Base to pass DeclarativeBase to the constructor """
+    pass
+db = SQLAlchemy(model_class=Base)
+
+# Create the app
 app = Flask(__name__)
 
-all_books = []
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
 
+# Attach SQLAlchemy to Flask
+db.init_app(app)
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
 @app.route('/')
 def home():
     """ Home of the Virtual Bookshelf """
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars().all()
     return render_template("index.html", all_books=all_books)
 
 
@@ -29,15 +50,28 @@ def home():
 def add():
     """ Form to add a new book """
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"].strip(),
-            "author": request.form["author"].strip(),
-            "rating": request.form["rating"].strip(),
-        }
-        all_books.append(new_book)
+        title= request.form["title"].strip()
+        author = request.form["author"].strip()
+        rating_raw = request.form["rating"].strip()
+
+        # Minimal input hygiene
+        if not title or not author or not rating_raw:
+            return render_template("add.html")
+
+        new_book = Book(
+            title=title,
+            author=author,
+            rating=float(rating_raw),
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
         return redirect(url_for("home"))
     return render_template("add.html")
 
-
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     app.run(debug=True)
