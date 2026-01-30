653467ec26a6c04b9579a8887ad8402b7920123d
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-01-30 17:36:31 +0100

    (day_64): added ability to edit rating and review fields of a movie

--- day_64/main.py
+++ day_64/main.py
@@ -2,12 +2,12 @@
 Top 10 movies shown in an d-flex html page using Bootstrap framework
 '''
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SubmitField
+from wtforms.validators import DataRequired, InputRequired
 
 from extensions import db
 from models import Movie  # pylint: disable=unused-import
@@ -27,7 +27,11 @@ Bootstrap5(app)
 db.init_app(app)
 
 # CREATE TABLE
-
+class RateMovieForm(FlaskForm):
+    """ WTForm used to edit the movie's rating and review """
+    rating = FloatField("Your Rating out of 10 e.g. 7.5", validators=[InputRequired()])
+    review = StringField("Your Review", validators=[DataRequired()])
+    submit = SubmitField("Done")
 
 @app.route("/")
 def home():
@@ -38,6 +42,32 @@ def home():
     all_movies = result.scalars().all()
 
     return render_template("index.html", movies=all_movies)
+
+@app.route("/edit/<int:movie_id>", methods=["GET", "POST"])
+def edit(movie_id):
+    """
+    Edit rating and review of a movie.
+
+    Using the ID as the PRIMARY KEY in the URL, the movie is identified
+
+    GET -> show the form prefilled with the current rating and revew
+    POST -> update the loaded object with commit
+    """
+
+    movie = db.get_or_404(Movie, movie_id)
+
+    form = RateMovieForm()
+
+    if form.validate_on_submit():
+        rating = form.rating.data
+        review = form.review.data.strip()
+
+        movie.rating = rating
+        movie.review = review
+        db.session.commit()
+
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=form)
 
 
 if __name__ == '__main__':
