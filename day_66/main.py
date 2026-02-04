489dee1f6af726290f3adaf80535abc0789bdefe
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-02-04 15:24:34 +0100

    (day_66): created /add route and tested with POSTMAN successfully

--- day_66/main.py
+++ day_66/main.py
@@ -11,9 +11,9 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
-from ast import List
+from hmac import new
 import random
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
@@ -44,6 +44,13 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        """
+        Use the dict comprehension to build dynamically
+        a dict of column names and their attribute
+        """
+        return { column.name : getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -61,22 +68,22 @@ def get_random_cafe():
     cafe_db = db.select(Cafe)
     result = db.session.execute(cafe_db)
     all_cafes = result.scalars().all()
-    cafe = random.choice(all_cafes)
+    random_cafe = random.choice(all_cafes)
 
-    cafe_json = jsonify(cafe={
-        "id": cafe.id,
-        "name": cafe.name,
-        "map_url": cafe.map_url,
-        "img_url": cafe.img_url,
-        "location": cafe.location,
-        "seats": cafe.seats,
-        "has_toilet": cafe.has_toilet,
-        "has_wifi": cafe.has_wifi,
-        "has_sockets": cafe.has_sockets,
-        "can_take_calls": cafe.can_take_calls,
-        "coffee_price": cafe.coffee_price,
-    })
-    return cafe_json
+    # cafe_json = jsonify(cafe={
+    #     "id": cafe.id,
+    #     "name": cafe.name,
+    #     "map_url": cafe.map_url,
+    #     "img_url": cafe.img_url,
+    #     "location": cafe.location,
+    #     "seats": cafe.seats,
+    #     "has_toilet": cafe.has_toilet,
+    #     "has_wifi": cafe.has_wifi,
+    #     "has_sockets": cafe.has_sockets,
+    #     "can_take_calls": cafe.can_take_calls,
+    #     "coffee_price": cafe.coffee_price,
+    # })
+    return jsonify(cafe=random_cafe.to_dict())
 
 @app.route("/all", methods=["GET"])
 def get_all_cafes():
@@ -84,26 +91,67 @@ def get_all_cafes():
     result = db.session.execute(cafe_db)
     all_cafes = result.scalars().all()
 
-    all_cafes_dict = []
-    for cafe in all_cafes:
-        cafe_item = {
-        "id": cafe.id,
-        "name": cafe.name,
-        "map_url": cafe.map_url,
-        "img_url": cafe.img_url,
-        "location": cafe.location,
-        "seats": cafe.seats,
-        "has_toilet": cafe.has_toilet,
-        "has_wifi": cafe.has_wifi,
-        "has_sockets": cafe.has_sockets,
-        "can_take_calls": cafe.can_take_calls,
-        "coffee_price": cafe.coffee_price,
-        }
-        all_cafes_dict.append(cafe_item)
+    # all_cafes_dict = []
+    # for cafe in all_cafes:
+    #     cafe_item = {
+    #     "id": cafe.id,
+    #     "name": cafe.name,
+    #     "map_url": cafe.map_url,
+    #     "img_url": cafe.img_url,
+    #     "location": cafe.location,
+    #     "seats": cafe.seats,
+    #     "has_toilet": cafe.has_toilet,
+    #     "has_wifi": cafe.has_wifi,
+    #     "has_sockets": cafe.has_sockets,
+    #     "can_take_calls": cafe.can_take_calls,
+    #     "coffee_price": cafe.coffee_price,
+    #     }
+    #     all_cafes_dict.append(cafe_item)
 
-    all_cafes_json = jsonify(cafes=all_cafes_dict)
-    return  all_cafes_json
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+@app.route("/search", methods=["GET"])
+def get_cafe_at_location():
+    """Use the Flask request.args to get the parameter from the URL
+
+    Returns:
+        json: a Response with a json file list of cafes in that location otherwise a 404 error
+    """
+    query_location = request.args.get("loc")
+
+    if query_location is None:
+        return jsonify(error={"Bad Request": "Missing 'loc' parameter"}), 400
+
+    cafe_all = db.select(Cafe)
+    result = db.session.execute(cafe_all.where(Cafe.location == query_location))
+    all_cafes = result.scalars().all()
+
+    if all_cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location"}), 404
+
+
 # HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"), # type: ignore[reportCallIssue]
+        map_url=request.form.get("map_url"), # type: ignore[reportCallIssue]
+        img_url=request.form.get("img_url"), # type: ignore[reportCallIssue]
+        location=request.form.get("location"), # type: ignore[reportCallIssue]
+        seats=request.form.get("seats"), # type: ignore[reportCallIssue]
+        has_toilet=bool(request.form.get("has_toilet")), # type: ignore[reportCallIssue]
+        has_wifi=bool(request.form.get("has_wifi")), # type: ignore[reportCallIssue]
+        has_sockets=bool(request.form.get("has_sockets")), # type: ignore[reportCallIssue]
+        can_take_calls=bool(request.form.get("can_take_calls")), # type: ignore[reportCallIssue]
+        coffee_price=request.form.get("coffee_price"), # type: ignore[reportCallIssue]
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return jsonify(response={"success":"Successfully added the new cafe"})
+
 
 # HTTP PUT/PATCH - Update Record
 
