9b5f88182ae7223348e64f2ba5e340d134c90859
Zamir Hashorva <zamir.hashorva@gmail.com>
2026-02-04 19:38:13 +0100

    (day_67): wired teh database to home for all posts and created route for single post with variable in url

--- day_67/main.py
+++ day_67/main.py
@@ -11,7 +11,7 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -53,14 +53,16 @@ with app.app_context():
 @app.route('/')
 def get_all_posts():
     # TODO: Query the database for all the posts. Convert the data to a python list.
-    posts = []
+    post_db = db.select(BlogPost)
+    result = db.session.execute(post_db)
+    posts = result.scalars().all()
     return render_template("index.html", all_posts=posts)
 
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
